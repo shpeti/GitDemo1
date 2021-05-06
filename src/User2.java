2597dc2b3aa0c499a5a441055fb67efa0e3e0a6d
ADMIN <ADMIN@HP>
2021-05-06 02:27:54 -0700

    User2 is commited...

---
+++ src/User2.java
@@ -0,0 +1,11 @@
+
+public class User2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Shidhar");
+		System.out.println("santosh");
+	}
+
+}
